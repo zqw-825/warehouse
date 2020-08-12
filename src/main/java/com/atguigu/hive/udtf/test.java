40e4d2011a9e2ca1d32c47b62399e1580e9c0673
zqw-825 <zqw965683596@163.com>
2020-08-12 18:29:07 +0800

    wh1

---
+++ src/main/java/com/atguigu/hive/udtf/test.java
@@ -0,0 +1,14 @@
+package com.atguigu.hive.udtf;
+
+import javax.sound.midi.Soundbank;
+import java.util.zip.CheckedOutputStream;
+
+/**
+ * @author zqw
+ * @create 2020-08-12 18:26
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("edsffsadfads");
+    }
+}
